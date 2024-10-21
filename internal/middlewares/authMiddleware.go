3ad2c546eda7f9b19e2fb95a9138f96333c09b59
abneribeiro <eliasabner38@gmail.com>
2024-10-21 20:37:52 +0100

    Ref: authMiddleware.go to improve code readability and add user authorization middleware

--- internal/middlewares/authMiddleware.go
+++ internal/middlewares/authMiddleware.go
@@ -1,55 +1,39 @@
 package middlewares
 
 import (
-	"fmt"
+	"go-reserve/internal/database"
+	"go-reserve/internal/models"
+	"log"
 	"net/http"
-	"os"
 	"strings"
 
-	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
 
-
-func TokenAuthMiddleware() gin.HandlerFunc {
+func AuthorizeUserMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Obtém o valor do cabeçalho "Authorization"
-		tokenString := c.Request.Header.Get("Authorization")
-		
-		// Verifica se o token foi fornecido
-		if tokenString == "" {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization token not provided"})
+		token := c.GetHeader("Authorization")
+		if token == "" {
+			log.Printf("Authorization header is required |%v|", token)
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is required"})
 			c.Abort()
 			return
 		}
 
-		// Verifica se o token começa com "Bearer "
-		if !strings.HasPrefix(tokenString, "Bearer ") {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid authorization token format"})
+		token = strings.TrimPrefix(token, "Bearer ")
+
+		dbService := c.MustGet("db").(database.Service)
+		db := dbService.GetDB()
+
+		user, err := models.ValidateToken(db, token)
+
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
 			c.Abort()
 			return
 		}
 
-		// Remove o prefixo "Bearer " para obter apenas o token
-		tokenString = strings.TrimPrefix(tokenString, "Bearer ")
-
-		// Valida o token JWT
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			// Verifica se o método de assinatura é esperado (neste caso, HMAC)
-			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-			}
-			// Retorna a chave secreta
-			return []byte(os.Getenv("SECRET_KEY")), nil
-		})
-
-		if err != nil || !token.Valid {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid authorization token"})
-			c.Abort()
-			return
-		}
-
-		// Se o token for válido, prossegue
+		c.Set("userId", user.ID)
 		c.Next()
 	}
 }
